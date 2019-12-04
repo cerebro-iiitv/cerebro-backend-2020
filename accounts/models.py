e4e37b4b8b098e930fcb880e87ca9a762a6de312
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-12-04 14:50:32 +0530

    accounts app created

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.core.mail import send_mail
+
+# class User(AbstractBaseUser, PermissionsMixin):
+
