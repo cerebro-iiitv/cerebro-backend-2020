96860708f11b705aa2a55136a2c19c9270df37a6
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-02-25 05:31:07 +0000

    allowed host added

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '*54i6wa4$)k@jukjjmydy=dsf!loj7eby4yyu#0mnum329k^b$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yashshah2820.pythonanywhere.com']
 
 
 # Application definition
